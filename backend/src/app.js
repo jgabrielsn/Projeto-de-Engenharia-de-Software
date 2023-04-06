3f4490ae1117a3a38b837b5c88f88c407aa176dd
diego-calixto <dhvc@cin.ufpe.br>
2023-04-06 15:52:25 -0300

    implementação de sessions

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = require('./router');
+const session = require('express-session');
+const sessionStore = require('./models/session');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(session({
+    key: 'session_cookie_name',
+    secret: 'session_cookie_secret',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}));
+
+
 app.use(router);
  
 module.exports = app;
