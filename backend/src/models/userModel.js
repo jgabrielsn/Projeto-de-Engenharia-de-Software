e75a6c3ac7720be7f61807ea99111370ab092d5f
diego-calixto <dhvc@cin.ufpe.br>
2023-04-13 19:13:49 -0300

    nova database

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -6,29 +6,29 @@ const getAll = async () => {
 };
 
 const createUser = async (user) => {
-    const { name } = user;
+    const { UserName } = user;
 
-    const query = 'INSERT INTO users(name, created_at) VALUES (?, ?)';
+    const query = 'INSERT INTO users(UserName, created_at) VALUES (?, ?)';
 
     const dateUTC = new Date(Date.now()).toUTCString();
     
-    const [createdUser] = await connection.execute(query, [name, dateUTC]);
+    const [createdUser] = await connection.execute(query, [UserName, dateUTC]);
     return {insertId : createdUser.insertId};
 };
 
 const deleteUser = async (id) => {
-    const query = 'DELETE FROM users WHERE id = ?';
+    const query = 'DELETE FROM users WHERE UserID = ?';
 
     const [removedUser] = await connection.execute(query, [id]);
     return removedUser;
 };
 
 const updateUser = async (id, user) => {
-    const { name } = user;
+    const { UserName } = user;
 
-    const query = 'UPDATE users SET name = ? WHERE id = ?';
+    const query = 'UPDATE users SET UserName = ? WHERE UserID = ?';
     
-    const [updatedUser] = await connection.execute(query, [name, id]);
+    const [updatedUser] = await connection.execute(query, [UserName, id]);
     return updatedUser;
 };
 
